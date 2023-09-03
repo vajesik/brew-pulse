cd2ccb13e49a1b880e60209b71f23ebc332ff177
Vanessa Jesik <vajesik@gmail.com>
2023-09-03 17:00:27 -0600

    components

---
+++ src/Brewery.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function Brewery() {}
+
+export default Brewery;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { render } from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App.js";
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));

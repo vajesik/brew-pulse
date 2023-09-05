9c778a75afc9fc48293292ddb16f9ca826d588a2
Vanessa Jesik <vajesik@gmail.com>
2023-09-05 13:27:40 -0600

    functioning routes appropriately styled in navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home.js";
+import About from "./About.js";
 import BreweryList from "./BreweryList";
+import Search from "./Search.js";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <BreweryList />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/featured_breweries" element={<BreweryList />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </div>
   );
 }

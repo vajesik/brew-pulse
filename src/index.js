5319186b09afbb87d96a46c4cb1961a810c2f092
Rae Stanton <raestanton93@gmail.com>
2023-09-05 09:53:17 -0700

    Adds react router to application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.js";
 import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter as Router} from "react-router-dom"
 
 const container = document.getElementById("root");
 const root = createRoot(container);

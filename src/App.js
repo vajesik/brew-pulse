74f3d0a8a0b14ee48a9b304d70281186cf70783b
Rae Stanton <raestanton93@gmail.com>
2023-09-06 09:48:23 -0700

    Adds bootstrap form for Reviews
    This adds a new Review route with a form. Also allows you to select your city, then find the brewery for that city you want to review. We can then show reviews somewhere.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./Home.js";
 import About from "./About.js";
 import TownList from "./TownList";
 import Search from "./Search.js";
+import Review from "./Review.js";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/featured_breweries" element={<TownList />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </div>
   );
